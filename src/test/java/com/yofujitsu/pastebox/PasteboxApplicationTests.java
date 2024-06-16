96ff126ecffd6d7e734eb1b337cd002e8e9d2657
yofujitsu <yofujitsuuu@gmail.com>
2024-06-16 16:52:40 +0300

    init

---
+++ src/test/java/com/yofujitsu/pastebox/PasteboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yofujitsu.pastebox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasteboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
